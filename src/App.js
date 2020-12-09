226abd82ecce5e85182a03fb118ab4a398960c08
AAKASH1598 <aakashdevajohn07@gmail.com>
2020-12-10 03:28:20 +0530

    Builds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import youtube from './api/youtube'
+import {SearchBar, VideoDetail , VideoList } from './components'
 
 class App extends React.Component{
+
+    state={
+        videos: [],
+        selectedVideo: null,
+    }
+    componentDidMount(){
+        this.handleSubmit('Kadhal ondru Kanden')
+    }
+
+    onVideoSelect =(video) =>{
+        this.setState({selectedVideo: video})
+    }
+
+    handleSubmit= async (searchTerm)=>{
+        const response =await  youtube.get('search', {params: {q : searchTerm}})
+
+
+        this.setState({videos: response.data.items , selectedVideo: response.data.items[0]})
+    }
+
     render(){
+        const {selectedVideo, videos } = this.state
         return(
-        <h1>YT Clone</h1>
+     
+        <Grid justify= 'center' container spacing={10}>
+            <Grid item xs={12}>
+                <Grid container spacing={10}>
+                    <Grid item xs={12}>
+                        <SearchBar onFormSubmit={this.handleSubmit}/>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <VideoDetail video={selectedVideo}/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+        </Grid>
         )
     }
 }
